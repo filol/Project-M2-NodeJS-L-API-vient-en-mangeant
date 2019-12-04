eeaa148a3baf9e773a5ea4d20f81f085a6d08250
griboue <gregoire.monet@gmail.com>
2019-12-04 17:15:53 +0100

    improved SandBoxPage design

--- front/src/_helpers/router.js
+++ front/src/_helpers/router.js
@@ -4,6 +4,7 @@ import Router from 'vue-router'
 import HomePage from '../components/home/HomePage.vue'
 import LoginPage from '../components/login/LoginPage.vue'
 import RegisterPage from '../components/login/RegisterPage.vue'
+import SandBox from '../components/game/SandBoxPage.vue'
 
 Vue.use(Router)
 
@@ -13,6 +14,7 @@ export const router = new Router({
     { path: '/', component: HomePage },
     { path: '/login', component: LoginPage },
     { path: '/register', component: RegisterPage },
+    { path: '/sandbox', component: SandBox },
 
     // otherwise redirect to home
     { path: '*', redirect: '/' }
--- front/src/components/game/SandBoxPage.vue
+++ front/src/components/game/SandBoxPage.vue
@@ -1,5 +1,43 @@
 <template>
-  <div></div>
+  <v-container>
+    <v-card>
+      <v-row no-gutters justify="center" align="center" class="text-center">
+        <v-col cols="12" class="mt-3">
+          <v-btn fab>
+            <v-icon>mdi-play</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+
+      <v-row no-gutters justify="center" align="center">
+        <v-col cols="12" sm="8" md="8" lg="4">
+          <v-card-text>
+            <v-text-field
+              v-model="username"
+              type="text"
+              label="Your guess"
+              :rules="[required]"
+              outlined
+              shaped
+            ></v-text-field>
+          </v-card-text>
+        </v-col>
+      </v-row>
+
+      <v-row no-gutters justify="center" align="center">
+        <v-col cols="6" sm="6" md="6" lg="6" align="right">
+          <div class="my-2">
+            <v-btn large>I don't know</v-btn>
+          </div>
+        </v-col>
+        <v-col cols="6" sm="6" md="6" lg="6">
+          <div class="my-2">
+            <v-btn large color="primary">Validate</v-btn>
+          </div>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
