7f58a45645391965d5f748d260cc357e414e8ee0
griboue <gregoire.monet@gmail.com>
2019-12-11 20:36:00 +0100

    improved homepage design

--- front/src/components/home/HomePage.vue
+++ front/src/components/home/HomePage.vue
@@ -2,11 +2,15 @@
   <div class="myDiv">
     <div class="bg"></div>
     <v-container>
-      <v-card flat color="transparent">
-        <v-card-title>
-          <v-row align="center" justify="center">Welcome to the Limbia project !</v-row>
-        </v-card-title>
-      </v-card>
+      <v-row no-gutters justify="center" align="center" class="text-center mt-12">
+        <v-col xs="0" sm="2"></v-col>
+        <v-col xs="12" sm="4" class="mt-12 pt-6">
+          <h1>Limba</h1>
+          <p>The best way to learn a new language</p>
+          <v-btn to="/game/sandbox" x-large color="success" dark>Try it now</v-btn>
+        </v-col>
+        <v-col xs="0" sm="6"></v-col>
+      </v-row>
     </v-container>
   </div>
 </template>
@@ -33,9 +37,9 @@ export default {
   bottom: 0;
   left: 0;
   right: 0;
-  background: url(https://cue.org/wp-content/uploads/2018/09/CUE-Craft-2-e1536698468391.jpg)
+  background: url(http://www.appwrap.org/wp-content/uploads/2017/01/mobile-learn.jpg)
     center center;
-  opacity: 0.4;
+  opacity: 0.15;
   width: 100%;
 }
 </style>
