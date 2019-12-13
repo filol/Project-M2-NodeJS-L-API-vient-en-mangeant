08f3f48c55c35762de090ddbddb8c2e98b61ed85
griboue <gregoire.monet@gmail.com>
2019-12-13 20:03:59 +0100

    finished the sandbox mode

--- front/src/components/game/SandBoxPage.vue
+++ front/src/components/game/SandBoxPage.vue
@@ -1,10 +1,43 @@
 <template>
   <v-container>
-    <!-- invisible audio player -->
+    <!-- Bad user answer Snackbar -->
+    <v-snackbar v-model="wrongSnackbar" :vertical="vertical" :timeout="2000">
+      <v-row align="center" justify="center">
+        <h1>"{{ previousWord }}"</h1>
+      </v-row>
+      <v-row align="center" justify="center">
+        <v-img
+          src="https://img.pngio.com/wrong-icon-with-png-and-vector-format-for-free-unlimited-download-wrong-png-512_512.png"
+          aspect-ratio="1"
+          max-width="100"
+          max-height="100"
+        ></v-img>
+      </v-row>
+    </v-snackbar>
+
+    <!-- Good user answer Snackbar -->
+    <v-snackbar v-model="correctSnackbar" :vertical="vertical" :timeout="1000">
+      <v-row align="center" justify="center">
+        <v-img
+          src="https://img.pngio.com/validate-input-data-using-validator-in-vapor-20-swiftyjimmy-validate-png-270_270.png"
+          aspect-ratio="1"
+          max-width="100"
+          max-height="100"
+        ></v-img>
+      </v-row>
+    </v-snackbar>
+
+    <!-- Loading overlay animation -->
+    <v-overlay :value="overlay">
+      <v-progress-circular class :size="100" :width="7" color="green" indeterminate></v-progress-circular>
+    </v-overlay>
+
+    <!-- Invisible audio player -->
     <audio id="audioPlayback" @ended="endPlaying()" style="display:none;" controls>
       <source id="audioSource" type="audio/mp3" src />
     </audio>
 
+    <!-- Game instructions -->
     <v-row no-gutters justify="center" align="center" class="text-center">
       <v-col cols="12">
         <h1>Sandbox</h1>
@@ -13,6 +46,19 @@
     </v-row>
     <v-card>
       <v-row no-gutters justify="center" align="center" class="text-center">
+        <v-col class="d-flex" cols="12" sm="2">
+          <v-select
+            v-on:change="generateNewGame"
+            v-model="selectedLanguage"
+            :items="lanuageIds"
+            label="en-US"
+            solo
+          ></v-select>
+        </v-col>
+      </v-row>
+
+      <!-- Play button -->
+      <v-row no-gutters justify="center" align="center" class="text-center">
         <v-col cols="12" class="mt-3">
           <v-btn v-on:click="play()" fab>
             <v-icon>{{ buttonIcon }}</v-icon>
@@ -20,6 +66,7 @@
         </v-col>
       </v-row>
 
+      <!-- User text input -->
       <v-row no-gutters justify="center" align="center">
         <v-col cols="12" sm="8" md="8" lg="4">
           <v-card-text>
@@ -28,13 +75,9 @@
         </v-col>
       </v-row>
 
+      <!-- Validate button -->
       <v-row no-gutters justify="center" align="center">
-        <v-col cols="6" sm="6" md="6" lg="6" align="right">
-          <div class="my-2">
-            <v-btn large>I don't know</v-btn>
-          </div>
-        </v-col>
-        <v-col cols="6" sm="6" md="6" lg="6">
+        <v-col cols="12" justify="center" align="center">
           <div class="my-2">
             <v-btn v-on:click="validateAnswer()" large color="primary">Validate</v-btn>
           </div>
@@ -45,23 +88,35 @@
 </template>
 
 <script>
-import Vue from 'vue'
-var AWS = require('aws-sdk/dist/aws-sdk-react-native')
+// Dictionary of nearly 2600 english words
+import json from '../../assets/dictionary.json'
 
+// Importing the AWS SDK (for translation or pronunciation)
+var AWS = require('aws-sdk/dist/aws-sdk-react-native')
 AWS.config.region = 'us-east-1'
 AWS.config.credentials = new AWS.CognitoIdentityCredentials({
   IdentityPoolId: 'us-east-1:d5beba94-b05a-49ce-b90c-a2c2ad452e44'
 })
+var translate = new AWS.Translate({ apiVersion: '2017-07-01' })
 
 export default {
   name: 'App',
   data: () => ({
+    lanuageIds: ['fr-FR', 'es-ES', 'it-IT', 'de-DE', 'en-US'],
+    voiceIds: ['Mathieu', 'Enrique', 'Giorgio', 'Hans', 'Matthew'],
     buttonIcon: 'mdi-play',
     wordToGuess: '',
+    previousWord: '',
     word: '',
-    timeToReadWord: 1500
+    selectedLanguage: 'en-US',
+    overlay: false,
+    correctSnackbar: false,
+    wrongSnackbar: false,
+    vertical: true,
+    dictionary: json
   }),
   methods: {
+    // Play the word audio
     play () {
       if (this.buttonIcon === 'mdi-play') {
         this.buttonIcon = 'mdi-pause'
@@ -71,24 +126,29 @@ export default {
     endPlaying () {
       this.buttonIcon = 'mdi-play'
     },
+
+    // Validate an answer, display if good or wrong and generate next game
     validateAnswer () {
-      console.log(this.word)
+      this.previousWord = this.wordToGuess
       if (this.word === this.wordToGuess) {
-        alert('GAGNE !')
+        this.correctSnackbar = true
       } else {
-        alert('PERDU ! the word was: ' + this.wordToGuess)
+        this.wrongSnackbar = true
       }
       this.generateNewGame()
     },
 
+    // Get the mp3 of the 'wordToGuess' and make it ready to play
     loadNewWordAudio () {
+      var ref = this
       // Create the JSON parameters for getSynthesizeSpeechUrl
       var speechParams = {
         OutputFormat: 'mp3',
         SampleRate: '16000',
         Text: '',
         TextType: 'text',
-        VoiceId: 'Matthew'
+        LanguageCode: this.selectedLanguage,
+        VoiceId: this.voiceIds[this.lanuageIds.indexOf(this.selectedLanguage)]
       }
       speechParams.Text = this.wordToGuess
 
@@ -103,19 +163,27 @@ export default {
         } else {
           document.getElementById('audioSource').src = url
           document.getElementById('audioPlayback').load()
+          ref.overlay = false // hidding the loading overlay
         }
       })
     },
-    generateNewGame () {
-      // select a new random word
-      Vue.axios
-        .get('https://random-word-api.herokuapp.com/word?key=9BG04C8Q&number=1')
-        .then(response => {
-          console.log(response.data)
-          this.wordToGuess = response.data[0]
-          this.word = ''
-          this.loadNewWordAudio()
-        })
+    async generateNewGame () {
+      this.overlay = true
+      var ref = this
+      var params = {
+        SourceLanguageCode: 'en', /* required */
+        TargetLanguageCode: this.selectedLanguage.substring(0, 2), /* required */
+        Text: this.dictionary.data[Math.floor(Math.random() * 2645)]/* required */
+      }
+      await translate.translateText(params, function (err, data) {
+        if (err) {
+          console.log(err, err.stack)
+        } else {
+          ref.wordToGuess = data.TranslatedText
+          ref.word = ''
+          ref.loadNewWordAudio()
+        }
+      })
     }
   },
   created () {
