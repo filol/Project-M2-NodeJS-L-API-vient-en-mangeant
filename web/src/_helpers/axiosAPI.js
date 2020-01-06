be04154dd36d534cc61ce960a4f70b31a215d8ba
CALIARI Axel <axel.caliari@efrei.net>
2020-01-06 19:24:59 +0100

    Web port

--- web/src/_helpers/axiosAPI.js
+++ web/src/_helpers/axiosAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const axiosAPI = axios.create({
-  baseURL: process.env.VUE_APP_API_URL || 'http://localhost:3001',
+  baseURL: process.env.VUE_APP_API_URL || 'http://localhost:5000',
   withCredentials: true,
   headers: {
     Authorization: '',
